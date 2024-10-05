b679efcfec6c55a1be01f887f9ad45b642a6af69
bgants <bgants@users.noreply.github.com>
2024-10-05 13:34:14 +0000

    getting to 100% test coverage

--- main.py
+++ main.py
@@ -22,7 +22,6 @@ def add_cli(a, b):
     click.echo(add(a, b))
 
 
-
 if __name__ == "__main__":
     # pylint: disable=no-value-for-parameter
     add_cli()
--- test_main.py
+++ test_main.py
@@ -3,8 +3,23 @@ Test goes here
 
 """
 
-from mylib.calculator import add
+from mylib.calculator import add, subtract, multiply, divide
 
 
 def test_add():
     assert add(1, 2) == 3
+
+
+# create a multiple test
+def test_multiply():
+    assert multiply(3, 4) == 12
+
+
+# create a subtract test
+def test_subtract():
+    assert subtract(5, 2) == 3
+
+
+# create a divide test
+def test_divide():
+    assert divide(10, 2) == 5
